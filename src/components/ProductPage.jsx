d7658ab76cc2b811acbd7af3065a354bf4cc5560
RomiMolina <roomi.molina@gmail.com>
2024-06-11 21:35:50 -0300

    add checkout

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -60,8 +60,7 @@ const ProductPage = () => {
                     </select>
                 </div>
                 <Link to={"/checkout"}>
-                <button onClick={()=> dispatch(addToCart(addQuantityToProduct()))} className="bg-yellow-400 w-full p-3 text-xs xl:text-sm rounded 
-                hover:bg-yellow-500 mt-3">Add to Cart</button>
+                <button onClick={()=> dispatch(addToCart(addQuantityToProduct()))} className="btn">Add to Cart</button>
                 </Link>
             </div>
         </div>
